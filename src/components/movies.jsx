c269eb2e2fc9f725564c33b683680b45c761945d
suhailphp <suhailphp@gmail.com>
2023-04-02 17:59:38 +0400

    Vidly movie list with delte option done

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,32 +6,51 @@ class Movies extends Component {
   };
   render() {
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Title</th>
-            <th scope="col">Genre</th>
-            <th scope="col">Stock</th>
-            <th scope="col">Rate</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.movies.map((movie) => {
-            return (
-              <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td></td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <>
+        <p>Total {this.state.movies.length} movie</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => {
+              return (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <button
+                      onClick={() => this.handleDelete(movie)}
+                      className="btn btn-sml btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
     );
   }
+
+  handleDelete = (movie) => {
+    let { movies } = this.state;
+    const index = movies.findIndex((obj) => obj === movie);
+    if (index !== -1) {
+      movies.splice(index, 1);
+      this.setState({ movies: movies });
+    }
+  };
 }
 
 export default Movies;
