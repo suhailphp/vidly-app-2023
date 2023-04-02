1e691aa5e2a22bfab5dfb6a850e7e5732ed1d4f0
suhailphp <suhailphp@gmail.com>
2023-04-02 15:34:50 +0400

    Vidly app craeted with getbootsrap website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <>
+      <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="/"
+          className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
         >
-          Learn React
+          <svg
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="bi me-2"
+            width="40"
+            height="32"
+          >
+            <path d="M3.5 3.75a.25.25 0 01.25-.25h13.5a.25.25 0 01.25.25v10a.75.75 0 001.5 0v-10A1.75 1.75 0 0017.25 2H3.75A1.75 1.75 0 002 3.75v16.5c0 .966.784 1.75 1.75 1.75h7a.75.75 0 000-1.5h-7a.25.25 0 01-.25-.25V3.75z" />
+            <path d="M6.25 7a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zm-.75 4.75a.75.75 0 01.75-.75h4.5a.75.75 0 010 1.5h-4.5a.75.75 0 01-.75-.75zm16.28 4.53a.75.75 0 10-1.06-1.06l-4.97 4.97-1.97-1.97a.75.75 0 10-1.06 1.06l2.5 2.5a.75.75 0 001.06 0l5.5-5.5z" />
+          </svg>
+
+          <span className="fs-4">Vidly app</span>
         </a>
+
+        <ul className="nav nav-pills">
+          <li className="nav-item">
+            <a href="#" className="nav-link active" aria-current="page">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              Features
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              Pricing
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              FAQs
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              About
+            </a>
+          </li>
+        </ul>
       </header>
-    </div>
+
+      <main className="container">
+        <h1>hello</h1>
+      </main>
+    </>
   );
 }
 
