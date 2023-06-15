a3236e2ded76d4a1470564a2ff2261a9d5e8e4fc
suhailphp <suhailphp@gmail.com>
2023-06-15 16:08:39 +0400

    Movie link to movie details page

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import _ from 'lodash'
 
 class TableBody extends Component {
+    renderContent =(item,column)=>{
+      if(column.link){
+        return column.link(item)
+      }
+      else if(column.path){
+        return _.get(item,column.path)
+      }
+      else{
+        return column.content(item)
+      }
+    }
     render() { 
         const {data,columns} = this.props
         return (
@@ -11,7 +22,10 @@ class TableBody extends Component {
                   <tr key={item._id}>
                     {columns.map(column=>(
                         <td key={item._id+(column.path||column.key)}>
-                            {column.path?_.get(item,column.path):column.content(item)}
+                            {
+                              //column.path?_.get(item,column.path):column.content(item)
+                              this.renderContent(item,column)
+                            }
                         </td>
                     ))}
                   </tr>
