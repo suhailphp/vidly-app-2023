71dea53ecc8d3eb9c9793267e130ca435b7c287e
suhailphp <suhailphp@gmail.com>
2023-06-16 20:40:05 +0400

    Validation added for form

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import Input from './common/input';
 class Login extends Component {
     state = {
-        account:{userName:'',password:''}
+        account:{userName:'',password:''},
+        error:{}
     } 
+    validate =()=>{
+        let error = {}
+        let {account} = this.state
+        if(account.userName === '')
+            error.userName = 'User name is required'
+        if(account.password === '')
+            error.password = 'Password is required'
+        return Object.keys(error).length === 0?null:error
+    }
     handleSubmit=(e)=>{
         e.preventDefault();
+        let error = this.validate()
+        if(error){
+            this.setState({error})
+            return false
+        } 
         console.log('submitted');
     }
     handleChange = (e)=>{
@@ -14,18 +29,23 @@ class Login extends Component {
         this.setState({account})
     }
     render() { 
+        let {account,error} = this.state
         return (
-            <form onSubmit={this.handleSubmit} >
+            <form onSubmit={this.handleSubmit} className='' >
                 <h1>Login Form</h1>
                 <Input
                     name='userName'
                     label='User Name'
+                    value={account.userName}
+                    error={(error.password)?error.password:null}
                     onChange={this.handleChange}
                 ></Input>
                 <Input
                     name='password'
                     label='Password'
                     type='password'
+                    value={account.password}
+                    error={(error.password)?error.password:null}
                     onChange={this.handleChange}
                 ></Input>
                 <button className="btn btn-primary">Submit</button>
