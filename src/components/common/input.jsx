d1b33852f4868bb801b6df07e47e1743cd00c82a
suhailphp <suhailphp@gmail.com>
2023-06-16 20:03:22 +0400

    onChange function problem fixed

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-const Input = ({name,id='',label,type='text',onChange}) => {
+const Input = ({name,id='',label,type='text',value,onChange}) => {
     id = (id)?id:name
     return ( 
         <div className="mb-1">
             <label htmlFor={id} className="form-label">{label}</label>
-            <input type={type} onChange={onchange} name={name} id={id} className="form-control" />
+            <input type={type} onChange={onChange} name={name} id={id} value={value} className="form-control" />
         </div>
      );
 }
